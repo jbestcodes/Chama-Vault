37a98674e2f27cca9df23a3cb8e60452dd2acdb7
Julie Adul <julieachola2@gmail.com>
2025-08-16 18:34:24 +0300

    updated the environment variables for database connection for deployment

--- Server/index.js
+++ Server/index.js
@@ -10,10 +10,10 @@ const PORT = process.env.PORT || 5000;
 
 // Create a connection pool once
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '28494217',
-    database: 'Chama_Vault',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
