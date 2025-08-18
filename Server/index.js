3de12db77bc50a5cbe67787c54bf358a738aa955
Julie Adul <julieachola2@gmail.com>
2025-08-18 12:55:14 +0300

    server config

--- Server/index.js
+++ Server/index.js
@@ -10,15 +10,10 @@ const PORT = process.env.PORT || 5000;
 
 // Create a connection pool once
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT, // Add this line if you have a custom port
+    uri: process.env.DATABASE_URL,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0,
-    ssl: { rejectUnauthorized: true } // Add this line for Railway SSL
+    queueLimit: 0
 });
 
 app.use(cors());
