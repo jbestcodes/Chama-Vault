20bb888b7a1f1b6e2781086084ea9212472848f1
Julie Adul <julieachola2@gmail.com>
2025-12-11 11:30:11 +0300

    fix: update CORS origin to include jazanyumba.online

--- Server/index.js
+++ Server/index.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['https://chama-vault-aiid.vercel.app', 'http://localhost:5173'],
+  origin: ['https://chama-vault-aiid.vercel.app', 'https://jazanyumba.online','http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
